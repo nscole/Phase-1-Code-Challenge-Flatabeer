260e89c192f29c89c62e9d03fd97109af32c2b12
Naomi Cole <naomisarahcole@gmail.com>
2022-07-05 17:38:12 +1000

    Added fetch beers function and domcontentloaded

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // Code here
+document.addEventListener("DomContentLoaded", () => {
+    fetchData();
+})
+
+// Fetch Beers
+function fetchData() {
+    fetch("http://localhost:3000/beers")
+    .then((resp) => resp.json())
+    .then((data) => renderBeers(data))
+}
